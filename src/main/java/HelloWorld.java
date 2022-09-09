74b54ed8db3cd672bae6a1361b0dd29eaaca7a5c
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-09 20:11:57 +0800

    AM 01 commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Description==>TODO
+ * BelongsProject==>git_test01
+ * BelongsPackage==>PACKAGE_NAME
+ * CreateTime==>2022-09-09 20:09:14
+ * Version==>1.0
+ * Author==>02雪乃赤瞳楪祈校条祭制作委员会 wyq_start
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello!!git");
+
+    }
+
+}
