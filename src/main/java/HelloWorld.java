9308b6afd8e0d8c167522af4ed59f461764a3e3a
AMchuAMchuAMchu <2466130840@qq.com>
2022-09-10 19:23:26 +0800

    AM 01 commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -10,7 +10,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("hello!!git");
+        System.out.println("hello!!git01");
+        System.out.println("hello!!git02");
 
     }
 
